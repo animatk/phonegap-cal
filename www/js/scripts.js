a4476c413814a4010190bfba28a4a9d2c0de33e2
Alejandro Santos <alejandro.santos@grupoiris.co>
2014-12-03 09:42:50 -0500

    cambio que implementa una cookie para almacenar temporalmente el token de facebook y retorna al sitio o al aplicativo.

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -30,6 +30,34 @@ $.fn.serializeObject = function()
    return o;
 };
 
+function setCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+ 
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+ 
+function deleteCookie(name) {
+    setCookie(name,"",-1);
+}
+ 
+//setCookie('prueba4', '{"id":3025,"url":"'+location.host+'","status": true}', 1);
+//var co = $.parseJSON( getCookie('prueba4') ); 
+ 
 
 function ak_buscalabel(form, ipt){
 	var str = ipt.attr('name');
@@ -351,6 +379,13 @@ $(function(){
 		ak_showtip( $input, $input.attr('placeholder'));
 	});
 	
+	var fbcookie = $.parseJSON( getCookie('fbtoken') ); 
+	
+	if(fbcookie != null){
+		//
+		SESSION['fbtoken'] = fbcookie.ide;
+	}
+	
 	if( !MOBILE )
 	{
 		
@@ -412,30 +447,10 @@ $(function(){
 		fecha1.removeClass('fecha-hora');
 		fecha2.removeClass('fecha-hora');
 	}
-	
-	
-	obtener_querys( window.location.href );
-	
 });
 
 
-function obtener_querys(url) {
-	console.log(url);
-	// Parse the OAuth data received from Facebook
-	var hash = decodeURIComponent(url.substr(url.indexOf('#') + 1)),
-	params = hash.split('&'),
-	oauthData = {};
-	params.forEach(function (param) {
-		var splitter = param.split('=');
-		oauthData[splitter[0]] = splitter[1];
-	});
-	
-	var fbtoken = oauthData['access_token'];
-	if (fbtoken) {
-		SESSION['fbtoken'] = fbtoken;
-	}
-	
-}
+
 
 function ak_navigate(from, to, effect){
 	var fx = (effect != undefined)? effect : 'toLeft';
